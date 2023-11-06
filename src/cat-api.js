e57548b7d553e5f59741121271be64c9321c0d6a
julia-pro <juliaszatkowska52@gmail.com>
2023-11-06 21:17:27 +0100

    total changes

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,14 +1,69 @@
-import axios from "axios";
+// Устанавливаем базовый URL для API
+export const BASE_URL = 'https://api.thecatapi.com/v1';
+// Устанавливаем ключ API
+export const API_KEY = 'live_VhTrCGP65dIGzY8rmbUl7pgkAFfxbHetBgABGPGnLaQCFWyU5T7IvvATQNKs3Uhk';
 
-axios.defaults.baseURL = "https://api.thecatapi.com/v1/";
-axios.defaults.headers.common["x-api-key"] = "live_TI5EAlbpezi3Ehi1gb46aHwnZKqI02rOCyjU4QL5D7gYwB43Dbx1o71UYxSwULZi"; 
-
+// Определяем функцию fetchBreeds для получения списка пород
 export function fetchBreeds() {
-  return axios.get('breeds')
-    .then(response => response.data);
+  // Формируем конечную точку URL для запроса списка пород
+  const endpoint = `${BASE_URL}/breeds`;
+
+  // Выполняем запрос с помощью функции fetch
+  return fetch(endpoint, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  })
+    .then(response => {
+      // Проверяем, успешен ли ответ
+      if (!response.ok) {
+        throw new Error('Failed to fetch breeds'); // Выбрасываем ошибку в случае неудачного запроса
+      }
+      // Преобразуем ответ в формат JSON
+      return response.json();
+    })
+    .then(data => {
+      // Преобразуем полученные данные, чтобы они содержали только id и name породы
+      return data.map(breed => ({
+        id: breed.id,
+        name: breed.name,
+      }));
+    });
 }
 
-export function fetchCatByBreed(id) {
-  return axios.get(`images/search?breed_ids=${id}`)
-    .then(response => response.data);
+// Определяем функцию fetchCatByBreed для получения информации о кошке по породе
+export function fetchCatByBreed(breedId) {
+  // Формируем конечную точку URL для запроса информации о кошке
+  const endpoint = `${BASE_URL}/images/search?breed_ids=${breedId}`;
+
+  // Выполняем запрос с помощью функции fetch
+  return fetch(endpoint, {
+    headers: {
+      'x-api-key': API_KEY,
+    },
+  })
+    .then(response => {
+      // Проверяем, успешен ли ответ
+      if (!response.ok) {
+        throw new Error('Failed to fetch cat by breed'); // Выбрасываем ошибку в случае неудачного запроса
+      }
+      // Преобразуем ответ в формат JSON
+      return response.json();
+    })
+    .then(data => {
+      // Проверяем, получены ли данные и есть ли хотя бы одна кошка
+      if (data && data.length > 0) {
+        // Извлекаем информацию о породе кошки из полученных данных
+        const catInfo = data[0].breeds[0];
+        // Возвращаем объект с информацией о кошке, включая URL изображения, имя породы, описание и характер
+        return {
+          imageUrl: data[0].url,
+          breedName: catInfo.name,
+          description: catInfo.description,
+          temperament: catInfo.temperament,
+        };
+      } else {
+        throw new Error('No cat found for the selected breed'); // Выбрасываем ошибку, если не найдено ни одной кошки для выбранной породы
+      }
+    });
 }
--- src/index.js
+++ src/index.js
@@ -1,78 +1,89 @@
+// Импорт необходимых модулей
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import * as catApi from './cat-api.js';
 
-import SlimSelect from 'slim-select'
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js'
+// Определение ссылок на HTML-элементы
+const breedSelect = document.querySelector('.breed-select');
+const loadingElement = document.querySelector('.loading');
+const errorElement = document.querySelector('.error');
+const catInfoContainer = document.querySelector('.cat-info');
+const loaderElement = document.querySelector('.loader');
 
-const selectEl = document.querySelector('.breed-select');
-const infoEl = document.querySelector('.cat-info');
-const wrapperLoaderEl = document.querySelector('.wrapper-loader');
-const loaderEl = document.querySelector('.loader');
+// Скрытие элементов загрузки и ошибки
+loadingElement.style.display = 'block';
+errorElement.style.display = 'none';
+// Обработчик события загрузки контента страницы
+document.addEventListener('DOMContentLoaded', () => {
+  // Инициализация SlimSelect для выбора породы
+  new SlimSelect({
+    select: 'breed-select',
+    showContent: 'down',
+    placeholder: 'Select a breed',
+    allowDeselect: true,
+    deselectLabel: '<span class="placeholder-text">Select a breed</span>',
+    searchable: false,
+  });
 
-function onLoad() {
-  wrapperLoaderEl.classList.remove('is-hidden');
-  infoEl.classList.add('is-hidden'); // Приховати div.cat-info
-  fetchBreeds()
-    .then(res => {
-      const markup = createOptions(res);
-      addMarkup(selectEl, markup);
-      new SlimSelect({
-        select: selectEl,
-      });
-    })
-    .catch(onError)
-    .finally(() => {
-      console.log('finally');
-      wrapperLoaderEl.classList.add('is-hidden');
-      infoEl.classList.remove('is-hidden'); // Показати div.cat-info після завершення запиту
+// Функция для получения информации о кошке по породе
+  function getCatInfoByBreed(selectedBreedId) {
+    // Показать элемент загрузки
+    loadingElement.style.display = 'block';
+
+  return catApi
+  .fetchCatByBreed(selectedBreedId)
+  .then((catInfo) => {
+    // Формирование HTML с информацией о кошке
+    const catInfoHTML = `
+      <img src="${catInfo.imageUrl}">
+      <p>Breed: ${catInfo.breedName}</p>
+      <p>Description: ${catInfo.description}</p>
+      <p>Temperament: ${catInfo.temperament}</p>
+    `;
+    // Вставка HTML с информацией о кошке в контейнер и отображение контейнера
+    catInfoContainer.innerHTML = catInfoHTML;
+    catInfoContainer.style.display = 'block';
+    // Скрытие элемента загрузки
+    loaderElement.style.display = 'none';
+  })
+  .catch((error) => {
+    console.error(error);
+    // Скрытие элемента загрузки и вывод уведомления об ошибке
+    loaderElement.classList.remove('visible');
+    Notiflix.Notify.failure('Произошла ошибка. Пожалуйста, повторите попытку позже.');
+    // Показать уведомление в случае ошибки загрузки
+    errorElement.style.display = 'block';
+  });
+  }
+
+// Обработчик события изменения выбора породы
+breedSelect.addEventListener('change', () => {
+  const selectedBreedId = breedSelect.value;
+  // Получение информации о кошке по выбранной породе
+  getCatInfoByBreed(selectedBreedId);
+  // Скрытие элементов загрузки и ошибки
+  loadingElement.style.display = 'none';
+  errorElement.style.display = 'none';
+});
+// Получение списка пород и заполнение выбора породы
+catApi.fetchBreeds()
+  .then((breeds) => {
+    breeds.forEach((breed) => {
+      // Создание и добавление элемента <option> в выбор породы
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      breedSelect.appendChild(option);
     });
-}
-
-onLoad();
-
-
-function createOptions(items = []) {
-  return items
-    .map(({ name, id }) => `<option value="${id}">${name}</option>`)
-    .join('');
-}
-
-function addMarkup(el, markup = '') {
-  el.innerHTML = markup;
-}
-
-selectEl.addEventListener('change', onChange);
-
-function onChange(evt) {
-  wrapperLoaderEl.classList.remove('is-hidden');
-  fetchCatByBreed(evt.target.value)
-    .then(([res]) => {
-      const { url, breeds } = res;
-      const [{name, description, temperament}] = breeds;
-      const markup = createBoxInfo({ url, name, description, temperament });
-      addMarkup(infoEl, markup)
+    // Включение выбора породы и скрытие элемента загрузки после успешной загрузки списка пород
+    breedSelect.disabled = false;
+    // Скрытие элемента загрузки
+    loadingElement.style.display = 'none';
   })
-    .catch(onError).finally(() => {
-
-    wrapperLoaderEl.classList.add('is-hidden');
-  })
-}
-
-function createBoxInfo({url, name, description, temperament}) {
-  return `<div class="left-col">
-          <img src="${url}" alt="" />
-        </div>
-<div class="right-col">
-          <h1>${name}</h1>
-          <p><strong>Temperament:</strong> ${temperament}</p>
-          <p><strong>Description:</strong> ${description}</p>
-        </div>`
-}
-
-function onError() {
-  Notify.failure('Oops! Something went wrong! Try reloading the page!');
-  clearCatInfo(); 
-}
-
-function clearCatInfo() {
-  infoEl.innerHTML = ''; // Очистити вміст div.cat-info
-}
+  .catch((error) => {
+    console.error(error);
+    // Вывод уведомления об ошибке и скрытие элемента загрузки в случае ошибки при загрузке списка пород
+    Notiflix.Notify.failure('Не удалось загрузить список пород. Пожалуйста, повторите попытку позже.');
+    loadingElement.style.display = 'none';
+  });
+});
